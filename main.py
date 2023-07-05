54d8c3d8e4da7cd30c12fc13ecf9cf92752081cf
Denis Shelestov <deniro9897@gmail.com>
2023-07-06 00:05:34 +0300

    readme update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from web3 import Web3
 import json, time
 
 w3 = Web3(Web3.HTTPProvider('https://polygon-rpc.com'))
+contract_storage_address = "0xB6b309Ae66A12d69259566220A2D0e35fE4bC556"
 
 def to_checksum_address(address: str) -> str:
     w3 = Web3()
@@ -12,7 +13,6 @@ def getNetworkName(network_id):
 		return "Polygon"
 	return "Unknown"
 
-contract_storage_address = "0xB6b309Ae66A12d69259566220A2D0e35fE4bC556"
 abi = [{"inputs":[{"internalType":"bytes32","name":"contractName","type":"bytes32"},{"internalType":"uint256","name":"networkId","type":"uint256"}],"name":"getContractAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"contractString","type":"string"},{"internalType":"uint256","name":"networkId","type":"uint256"}],"name":"getContractAddressViaName","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"networkId","type":"uint256"}],"name":"getContractListOfNetwork","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getNetworkLists","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"nameString","type":"string"}],"name":"stringToContractName","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"pure","type":"function"}]
 
 contract_address = to_checksum_address(contract_storage_address)
